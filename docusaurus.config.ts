6b1b04d722cd5d3154cfc473c4140b244682e9f4
Zahgrom34 <Zakhriw@gmail.com>
2024-07-27 19:22:33 +0500

    FIXES #2

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
   plugins: [
     [
-    "docusaurus2-dotenv",
+    "docusaurus-plugin-dotenv",
       {
         path: "./.env", // The path to your environment variables.
         systemvars: true, // Set to true if you would rather load all system variables as well (useful for CI purposes)
@@ -34,10 +34,9 @@ const config: Config = {
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'ru'],
-  },
+//   i18n: {
+//     defaultLocale: 'en',
+//   },
 
   presets: [
     [
@@ -78,10 +77,6 @@ const config: Config = {
         },
         
         {to: '/blog', label: 'Blog', position: 'left'},
-        {
-            type: 'localeDropdown',
-            position: 'right'
-        },
         {
             type: 'docsVersionDropdown',
             position: 'right'
