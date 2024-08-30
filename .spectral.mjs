0e25bb99203289e447e2781b3a736d4e00577bc2
Sevak Petrosian <info@rapidpaytrade.com>
2024-08-30 03:40:36 +0300

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI1NzEzOQ/spectral.js?branch_slug=main&token=5d8e3ad0-9350-46fb-b229-01f78553aea4";
+export default { extends: ruleset };
